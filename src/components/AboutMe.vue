6ceb580051878ad075ffdc0d2b49ff1d94cf504c
thanchatip <thanchatip.coe@gmail.com>
2022-01-27 21:26:50 +0700

    add information to about page
    - add information to components/about-me
    - add information to components/education

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,16 +1,11 @@
 <template>
 <div class="about-me">
     <div class="content">
-        <h1 class="display-1"><span class="half-background">About Me .</span></h1>    
-            <div class="display-4"><span class="half-background">Profile</span></div>
-            <div class="detail">
-                <h4>Name</h4>
-                <h4>Thanchanok Tipmaneekomut</h4>
-                </div>
-            <div class="detail">
-                <h4>Age</h4>
-                <h4>24</h4>
-            </div>
+        <h1 class="display-1"><span class="half-background">About Me .</span></h1>
+            <h3>
+                Hello, I'm Thanchanok Tipmaneekomut. You can call me <i>Tammie</i><br>
+                I'm a Software Developer with 2 years experience on working with MEVN stack<br>
+            </h3>  
     </div>
 </div>
 </template>
@@ -27,13 +22,24 @@ export default {
     position: sticky;
     width: 100%;
     height: 100vh;
-    background-color: antiquewhite;
+    background-color: #e7dce5;
+    vertical-align: middle;
 }
 
 
-.about-me h1 {
+.about-me h1, h3 {
     font-style: bold;
     color: #1c2729;
     text-align: center;
 }
+.about-me .content{
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%)
+}
+
+
 </style>
--- src/components/Education.vue
+++ src/components/Education.vue
@@ -3,9 +3,26 @@
     <div class="content">
         <h1 class="display-1"><span class="half-background">Education .</span></h1>
         <div class="detail">
-          <h4>University</h4>
+          <h5>December 2019</h5>
           <h4>Prince of Songkla University</h4>
+          <h4>Bachelor of Engineering</h4>
+          <h5>Computer Engineering</h5>
         </div>
+        <div class="detail">
+          <h5>December 20xx</h5>
+          <h4>Saparachinee Trang School</h4>
+          <h4>High School</h4>
+          <h5>Science - Math (SMTE) program </h5>
+        </div>
+
+        <div class="detail">
+          <h5>December 20xx</h5>
+          <h4>Horwang School</h4>
+          <h4>Junior High School</h4>
+          <h5>Intensive program </h5>
+        </div>
+
+
     </div>
   </div>
 </template>
@@ -20,12 +37,18 @@ export default {
 .education {
     width: 100%;
     height: 100vh;
-    background-color: rgb(224, 134, 16);
+    background-color: #cfcad1;
     position: static;
 }
 
 .education h1 {
     font-style: bold;
     color: #1c2729;
+}
+
+.education .detail {
+  padding: 2em;
+  background-color: lightseagreen;
+  margin: 2em 0 2em 0;
 }
 </style>
